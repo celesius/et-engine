d0ce4d98d960ecf6b597262b3ccdf5ec9a05d869
rezniksa@gmail.com <rezniksa@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2012-02-19 00:34:03 +0000

    # cocos integration

---
+++ trunk/include/et/platform/cocos/etnode.h
@@ -0,0 +1,17 @@
+//
+//  etnode.h
+//  pictorial2
+//
+//  Created by Sergey Reznik on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+#import <cocos2d.h>
+
+@interface etNode : CCNode
+{
+}
+
+@end
