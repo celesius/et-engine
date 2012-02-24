473984915bacebe3aa4766bbda2e6bacf868cdc0
ReznikSA@gmail.com <ReznikSA@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2012-02-24 21:42:40 +0000

    # test

--- trunk/src/platform/cocos/atapplication.cocos.mm
+++ trunk/src/platform/cocos/atapplication.cocos.mm
@@ -54,7 +54,7 @@ static etApplication* _sharedInstance = nil;
 
 - (void)loaded
 {
-	NSAssert(_loaded == NO, @"Method [etApplication loaded] should be called only once.");
+	NSAssert(_loaded == NO, @"Method [etApplication loaded] should be called once.");
 	
 	CCGLView* view = (CCGLView*)[[CCDirector sharedDirector] view];
 	NSAssert(view, @"Cocos OpenGL view should be initialized before running embedded application.");
