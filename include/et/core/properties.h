07b9726ef1dac96454d12e06e04a23fb8a3822a4
rezniksa <rezniksa@gmail.com>
2012-03-11 22:45:37 +0000

    # properties

---
+++ include/et/core/properties.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define ET_DECLARE_PROPERTY(T, name, setter) public: \
+		T name() const \
+			{ return _##name; } \
+		void setter(const T& value) \
+			{ _##name = value; } \
+	private: \
+		T _##name;
+
+#define ET_DECLARE_PROPERTY_READONLY(T, name) public: \
+		T name() const \
+			{ return _##name; } \
+	private: \
+		T _##name;
+
+#define ET_DECLARE_PROPERTY_READONLY_REF(T, name) public: \
+		const T& name() const \
+			{ return _##name; } \
+	private: \
+		T _##name;
