ac6da7a5bc1847334c527f50c2d65279c5d48897
rezniksa@gmail.com <rezniksa@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2012-06-05 21:44:48 +0000

    # IAP

---
+++ trunk/include/et/platform-ios/iap.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of `et engine`
+ * Copyright 2009-2012 by Sergey Reznik
+ * Please, do not modify contents without approval.
+ *
+ */
+
+#pragma once
+
+#include <set>
+#include <et/core/tools.h>
+
+namespace et
+{
+    class PurchasesManagerDelegate
+    {
+    public:
+        virtual ~PurchasesManagerDelegate() { }
+        
+        virtual void purchasesManagerDidCheckAvailableProducts(const StringList& products) { }
+        virtual void purchasesManagerDidFailToCheckAvailableProducts() { }
+        
+        virtual void purchasesManagerDidPurchaseProduct(const std::string& product) { }
+        virtual void purchasesManagerDidFailToPurchaseProduct(const std::string& product) { }
+        
+        virtual void purchasesManagerDidRestorePurchasedProduct(const std::string& product) { }
+        virtual void purchasesManagerDidFinishRestoringPurchases() { }
+        virtual void purchasesManagerDidFailToRestorePurchases() { }
+    };
+    
+    class PurchasesManager
+    {
+    public:
+        typedef std::set<std::string> ProductsSet;
+        
+    public:
+        PurchasesManager();
+        ~PurchasesManager();
+        
+        void checkAvailableProducts(const ProductsSet& products, PurchasesManagerDelegate* delegate);
+        bool purchaseProduct(const std::string& product, PurchasesManagerDelegate* delegate);
+    };
+}
