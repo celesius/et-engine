7199c1062fc60ab3941b2aa061499a3ee7e19da0
ReznikSA@gmail.com <ReznikSA@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2012-06-13 17:00:57 +0000

    # messagebox fix

--- trunk/src/gui/messageview.cpp
+++ trunk/src/gui/messageview.cpp
@@ -20,12 +20,13 @@ MessageView::MessageView(const std::string& text, Font font, const Image& image,
 	
 	_text = Label::Pointer(new Label(text, font, _background.ptr()));
 	_text->setPivotPoint(vec2(0.5f, 0.0f));
+	_text->setHorizontalAlignment(ElementAlignment_Center);
 	
 	_button1 = Button::Pointer(new Button(button1title, font, _background.ptr()));
 	_button1->clicked.connect(this, &MessageView::buttonClicked);
 	_button1->setPivotPoint(hasSecondButton() ? vec2(0.0f, 1.0f) : vec2(0.5f, 1.0f));
 	
-	_button2 = Button::Pointer(new Button(button1title, font, _background.ptr()));
+	_button2 = Button::Pointer(new Button(button2title, font, _background.ptr()));
 	_button2->clicked.connect(this, &MessageView::buttonClicked);
 	_button2->setPivotPoint(vec2(1.0f, 1.0f));
 	_button2->setVisible(hasSecondButton());
@@ -33,15 +34,16 @@ MessageView::MessageView(const std::string& text, Font font, const Image& image,
 
 void MessageView::layout(const vec2& sz)
 {
-	vec2 bgSize = maxv(_background->imageDescriptor().size, floorv(0.333f * sz));
+	vec2 bgSize = maxv(_background->imageDescriptor().size, floorv(vec2(0.9f * sz.x, 0.4f * sz.y)));
 	_background->setFrame(floorv(0.5f * sz), bgSize);
 
 	vec2 bOffset = floorv(0.15f * bgSize);
 	vec2 b1pos = hasSecondButton() ? vec2(bOffset.x, bgSize.y - bOffset.y) : vec2(0.5f * bgSize.x, bgSize.y - bOffset.y);
 	vec2 b2pos = bgSize - bOffset;
 	
-	_button1->setPosition(floorv(b1pos));
-	_button2->setPosition(floorv(b2pos));
+	vec2 maxButtonSize = maxv(_button1->size(), _button2->size());
+	_button1->setFrame(floorv(b1pos), maxButtonSize);
+	_button2->setFrame(floorv(b2pos), maxButtonSize);
 	
 	float topOrigin = etMin(_button1->origin().y, _button2->origin().y);
 	
@@ -66,11 +68,13 @@ void MessageView::setButtonBackground(const Image& img, ElementState s)
 void MessageView::setButtonTextColor(const vec4& color)
 {
 	_button1->setTextColor(color);
+	_button2->setTextColor(color);
 }
 
 void MessageView::setButtonPressedTextColor(const vec4& color)
 {
 	_button1->setTextPressedColor(color);
+	_button2->setTextPressedColor(color);
 }
 
 bool MessageView::hasSecondButton() const
