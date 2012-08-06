12eb6431143a6382674a400699d11fee72ce0051
rezniksa <rezniksa@gmail.com>
2012-08-06 10:24:48 +0000

    # iOS character generator fix

--- src/platform-ios/charactergenerator.ios.mm
+++ src/platform-ios/charactergenerator.ios.mm
@@ -12,11 +12,11 @@ const size_t defaultTextureSize = 1024;
 class et::gui::CharacterGeneratorPrivate
 {
 	public:
-		CharacterGeneratorPrivate(const std::string& face, size_t size);
+		CharacterGeneratorPrivate(const std::string& face, const std::string& boldFace, size_t size);
 		~CharacterGeneratorPrivate();
 
 		void updateTexture(RenderContext* rc, Texture texture);
-		void renderCharacter(NSString* value, const vec2i& position);
+		void renderCharacter(NSString* value, const vec2i& position, bool bold);
 
 	public:
 		std::string _fontFace;
@@ -31,11 +31,19 @@ class et::gui::CharacterGeneratorPrivate
 		CGColorSpaceRef _colorSpace;
 };
 
-CharacterGenerator::CharacterGenerator(RenderContext* rc, const std::string& face, size_t size) : _rc(rc),
-	_private(new CharacterGeneratorPrivate(face, size)), _face(face), _size(size)
+CharacterGenerator::CharacterGenerator(RenderContext* rc, const std::string& face, const std::string& boldFace, size_t size) : _rc(rc),
+	_private(new CharacterGeneratorPrivate(face, boldFace, size)), _face(face), _size(size)
 {
 	_texture = _rc->textureFactory().genTexture(GL_TEXTURE_2D, GL_RGBA, vec2i(defaultTextureSize), GL_RGBA, GL_UNSIGNED_BYTE, 
 		BinaryDataStorage(), face + "font");
+	
+	for (size_t i = 32; i < 128; ++i)
+		generateCharacter(i, false);
+
+	for (size_t i = 32; i < 128; ++i)
+		generateBoldCharacter(i, false);
+	
+	_private->updateTexture(rc, _texture);
 }
 
 CharacterGenerator::~CharacterGenerator()
@@ -43,7 +51,7 @@ CharacterGenerator::~CharacterGenerator()
 	delete _private;
 }
 
-CharDescriptor CharacterGenerator::generateCharacter(int value)
+CharDescriptor CharacterGenerator::generateCharacter(int value, bool updateTexture)
 {
 	wchar_t string[2] = { value, 0 };
     NSString* wString = [[NSString alloc] initWithBytes:string length:sizeof(string) encoding:NSUTF32LittleEndianStringEncoding];
@@ -51,8 +59,10 @@ CharDescriptor CharacterGenerator::generateCharacter(int value)
 
 	rect textureRect;
 	_private->_placer.place(vec2i(characterSize.width + 2, characterSize.height + 2), textureRect);
-	_private->renderCharacter(wString, vec2i(static_cast<int>(textureRect.left + 1), static_cast<int>(textureRect.top + 1)));
-	_private->updateTexture(_rc, _texture);
+	_private->renderCharacter(wString, vec2i(static_cast<int>(textureRect.left + 1), static_cast<int>(textureRect.top + 1)), false);
+	
+	if (updateTexture)
+		_private->updateTexture(_rc, _texture);
 
 	[wString release];
 	
@@ -67,13 +77,28 @@ CharDescriptor CharacterGenerator::generateCharacter(int value)
 	return desc;
 }
 
-CharDescriptor CharacterGenerator::generateBoldCharacter(int value)
+CharDescriptor CharacterGenerator::generateBoldCharacter(int value, bool updateTexture)
 {
+	wchar_t string[2] = { value, 0 };
+    NSString* wString = [[NSString alloc] initWithBytes:string length:sizeof(string) encoding:NSUTF32LittleEndianStringEncoding];
+    CGSize characterSize = [wString sizeWithFont:_private->_boldFont];
+	
+	rect textureRect;
+	_private->_placer.place(vec2i(characterSize.width + 2, characterSize.height + 2), textureRect);
+	_private->renderCharacter(wString, vec2i(static_cast<int>(textureRect.left + 1), static_cast<int>(textureRect.top + 1)), true);
+	
+	if (updateTexture)
+		_private->updateTexture(_rc, _texture);
+	
+	[wString release];
+	
 	CharDescriptor desc(value, CharParameter_Bold);
-
-	rect t;
-	_private->_placer.place(vec2i(10, 10), t);
-
+	
+	desc.origin = textureRect.origin() + vec2(1.0f);
+	desc.size = textureRect.size() - vec2(2.0f);
+	desc.uvOrigin = _texture->getTexCoord(textureRect.origin());
+	desc.uvSize = textureRect.size() / _texture->sizeFloat();
+	
 	_boldChars[value] = desc;
 	return desc;
 }
@@ -82,14 +107,18 @@ CharDescriptor CharacterGenerator::generateBoldCharacter(int value)
  * Private
  */
 
-CharacterGeneratorPrivate::CharacterGeneratorPrivate(const std::string& face, size_t size) :
+CharacterGeneratorPrivate::CharacterGeneratorPrivate(const std::string& face, const std::string& boldFace, size_t size) :
 	_fontFace(face), _fontSize(size),
 	_placer(vec2i(defaultTextureSize), true), _data(defaultTextureSize * defaultTextureSize * 4, 0)
 {
     NSString* cFace = [NSString stringWithCString:face.c_str() encoding:NSASCIIStringEncoding];
+    NSString* cBoldFace = [NSString stringWithCString:boldFace.c_str() encoding:NSASCIIStringEncoding];
+	
     _font = [[UIFont fontWithName:cFace size:static_cast<float>(size)] retain];
+    _boldFont = [[UIFont fontWithName:cBoldFace size:static_cast<float>(size)] retain];
 	
 	_colorSpace = CGColorSpaceCreateDeviceRGB();
+	
     _context = CGBitmapContextCreate(_data.data(), defaultTextureSize, defaultTextureSize,
 									 8, defaultTextureSize * 4, _colorSpace, kCGImageAlphaPremultipliedLast);
 
@@ -117,9 +146,9 @@ void CharacterGeneratorPrivate::updateTexture(RenderContext* rc, Texture texture
 	texture->updateDataDirectly(rc, vec2i(defaultTextureSize), static_cast<char*>(_data.raw()), _data.size());
 }
 
-void CharacterGeneratorPrivate::renderCharacter(NSString* value, const vec2i& position)
+void CharacterGeneratorPrivate::renderCharacter(NSString* value, const vec2i& position, bool bold)
 {
 	UIGraphicsPushContext(_context);
-	[value drawAtPoint:CGPointMake(position.x, position.y) withFont:_font];
+	[value drawAtPoint:CGPointMake(position.x, position.y) withFont:(bold ? _boldFont : _font)];
 	UIGraphicsPopContext();
 }
