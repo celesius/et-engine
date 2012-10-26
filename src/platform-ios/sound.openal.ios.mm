e1e593a11266d807af60e580dd6a5532b7a14805
rezniksa <rezniksa@gmail.com>
2012-10-26 18:45:35 +0000

    # native sound methods, ios files renamed

---
+++ src/platform-ios/sound.openal.ios.mm
@@ -0,0 +1,54 @@
+/*
+ * This file is part of `et engine`
+ * Copyright 2009-2012 by Sergey Reznik
+ * Please, do not modify contents without approval.
+ *
+ */
+
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <AudioToolbox/AudioToolbox.h>
+#include <et/sound/sound.h>
+
+using namespace et;
+using namespace audio;
+
+extern ALCdevice* getSharedDevice();
+extern ALCcontext* getSharedContext();
+
+void interruptListener(void *inClientData, UInt32 inInterruptionState)
+{
+	if (inInterruptionState == kAudioSessionBeginInterruption)
+	{
+		AudioSessionSetActive(false);
+		alcMakeContextCurrent(0);
+		alcSuspendContext(getSharedContext());
+	}
+	else if (inInterruptionState == kAudioSessionEndInterruption)
+	{
+		int restart = 0;
+		OSStatus error = AudioSessionSetActive(true);
+		while ((error != 0) && (++restart > 10))
+		{
+			sleep(1);
+			error = AudioSessionSetActive(true);
+		}
+		
+		if (error == 0)
+		{
+			alcMakeContextCurrent(getSharedContext());
+			alcProcessContext(getSharedContext());
+		}
+	}
+}
+
+void Manager::nativeInit()
+{
+	AudioSessionInitialize(nil, nil, interruptListener, nil);
+	AudioSessionSetActive(true);
+}
+
+void Manager::nativeRelease()
+{
+	AudioSessionSetActive(false);
+}
