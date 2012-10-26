4957b28e8100f793050a511e905cbb4fc717c321
rezniksa <rezniksa@gmail.com>
2012-10-26 20:02:32 +0000

    # native audio file for win

---
+++ src/platform-win/sound.openal.win.cpp
@@ -0,0 +1,11 @@
+/*
+ * This file is part of `et engine`
+ * Copyright 2009-2012 by Sergey Reznik
+ * Please, do not modify contents without approval.
+ *
+ */
+
+#include <et/sound/sound.h>
+
+void et::audio::Manager::nativeInit() { }
+void et::audio::Manager::nativeRelease() { }
