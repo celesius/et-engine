f29f4c909b15ee1dceae5d7952a741b99704dd2f
rezniksa <rezniksa@gmail.com>
2012-12-28 01:36:08 +0000

    # mac input improved

--- src/platform-ios/application.ios.mm
+++ src/platform-ios/application.ios.mm
@@ -132,3 +132,7 @@ size_t Application::memoryUsage() const
 	kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &size);
 	return (kerr == KERN_SUCCESS) ? info.resident_size : 0;
 }
+
+void Application::setTitle(const std::string &s)
+{
+}
