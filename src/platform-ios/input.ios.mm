44d2426a7503d7cc2e5dfb651158fb773c6b0a17
rezniksa <rezniksa@gmail.com>
2013-01-22 09:49:41 +0000

    # pointers fix for iOS

--- src/platform-ios/input.ios.mm
+++ src/platform-ios/input.ios.mm
@@ -11,7 +11,7 @@ using namespace et;
 
 PointerInputInfo Input::currentPointer() const
 {
-	return PointerInputInfo(PointerType_None, vec2(0.0f), vec2(0.0f), 0, 0, queryTime());
+	return PointerInputInfo(PointerType_None, vec2(0.0f), vec2(0.0f), vec2(0.0f), 0, queryTime());
 }
 
 bool Input::canGetCurrentPointerInfo() const
