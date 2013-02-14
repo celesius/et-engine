dd2653c359d23aed6a307cb7601cdb2b2a9b3224
rezniksa@gmail.com <rezniksa@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2013-02-14 16:29:13 +0000

    # android updates

--- trunk/demo/source/main.cpp
+++ trunk/demo/source/main.cpp
@@ -1,6 +1,17 @@
+#include <et/core/et.h>
 #include "maincontroller.h"
+
+#if (ET_PLATFORM_ANDROID)
+#	include <android_native_app_glue.h>
+#endif
 
 int main(int argc, char* argv[])
 {
 	return et::application().run(argc, argv);
+}
+
+void android_main(struct android_app* state)
+{
+	app_dummy();
+	main(0, 0);
 }
