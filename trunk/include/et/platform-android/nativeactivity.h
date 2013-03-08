45bf6befa7b10bf2df49418642f5ac4980d57b7b
rezniksa@gmail.com <rezniksa@gmail.com@b0f9e1d9-3376-47b0-50cd-22c11bfc97cb>
2013-03-08 00:31:12 +0000

    # somewhat working on android

--- trunk/include/et/platform-android/nativeactivity.h
+++ trunk/include/et/platform-android/nativeactivity.h
@@ -3,7 +3,4 @@
 #include <android/log.h>
 #include <android_native_app_glue.h>
 
-namespace et
-{
-
-}
+#define androidLog(...) { __android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__); }
