a50bfbedf3ea112effef422b244ee3fabda6ca5d
rezniksa <rezniksa@gmail.com>
2013-04-27 21:24:32 +0000

    # remove whitespace method

--- src/core/tools.cpp
+++ src/core/tools.cpp
@@ -315,3 +315,9 @@ namespace et
 
 }
 
+std::string et::removeWhitespace(const std::string& s)
+{
+	StringDataStorage result(s.size(), 0);
+	ET_ITERATE(s, auto, c, if (!isWhitespaceChar(c)) result.push_back(c));
+	return std::string(result.data());
+}
