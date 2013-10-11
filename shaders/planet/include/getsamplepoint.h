7c04e96a5c66f1f53e39dfa5a573f43e31b1d831
rezniksa <rezniksa@gmail.com>
2013-10-11 16:28:31 +0000

    # deleted some folders

--- shaders/planet/include/getsamplepoint.h
+++
@@ -1,4 +0,0 @@
-float fSampleLength = fFar / fSamples;
-float fScaledLength = fSampleLength * vScale.x;
-vec3 v3SampleRay    = v3Ray * fSampleLength;
-vec3 v3SamplePoint  = v3Start + v3SampleRay * 0.5;
